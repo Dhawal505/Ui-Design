cafc92c8b11c4595f3c1c72521ee072f42da971a
= <=>
2021-02-16 15:30:03 +0530

    created by dhawal

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:stream_cycle_trainer_app/profile.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+  runApp(MyApp());
+
+
+}
+
